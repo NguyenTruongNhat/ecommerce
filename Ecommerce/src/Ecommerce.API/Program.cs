677796f39bf334a2b4e1b8a4e7f0c8b38c85100a
Nhat.Nguyen <nhat.nguyen@ptnglobalcorp.com>
2025-11-19 16:41:16 +0700

    add Entity model and create migration.

--- Ecommerce/src/Ecommerce.API/Program.cs
+++ Ecommerce/src/Ecommerce.API/Program.cs
@@ -1,8 +1,17 @@
+using Ecommerce.Persistence.DependencyInjection.Extensions;
+using Ecommerce.Persistence.DependencyInjection.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// Configure Options and SQL
+builder.Services.ConfigureSqlServerRetryOptions(builder.Configuration.GetSection(nameof(SqlServerRetryOptions)));
+builder.Services.AddSqlConfiguration();
+
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
---
+++ Ecommerce/src/Ecommerce.Persistence/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Ecommerce.Domain.Entities;
+using Ecommerce.Domain.Entities.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Action = Ecommerce.Domain.Entities.Identity.Action;
+
+namespace Ecommerce.Persistence;
+public sealed class ApplicationDbContext : IdentityDbContext<AppUser, AppRole, Guid>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+        => builder.ApplyConfigurationsFromAssembly(AssemblyReference.Assembly);
+
+    public DbSet<AppUser> AppUses { get; set; }
+    public DbSet<Action> Actions { get; set; }
+    public DbSet<Function> Functions { get; set; }
+    public DbSet<ActionInFunction> ActionInFunctions { get; set; }
+    public DbSet<Permission> Permissions { get; set; }
+
+    public DbSet<Product> Products { get; set; }
+}
