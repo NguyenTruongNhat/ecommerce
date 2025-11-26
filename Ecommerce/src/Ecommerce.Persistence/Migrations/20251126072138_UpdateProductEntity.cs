b0cf2f4b3f92959ec4c0d8c880211c924e41ec4a
Nhat.Nguyen <nhat.nguyen@ptnglobalcorp.com>
2025-11-26 14:25:16 +0700

    #44: update product entity

---
+++ Ecommerce/src/Ecommerce.Persistence/Migrations/20251126072138_UpdateProductEntity.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ecommerce.Persistence.Migrations;
+
+/// <inheritdoc />
+public partial class UpdateProductEntity : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AlterColumn<string>(
+            name: "Variants",
+            table: "Product",
+            type: "nvarchar(max)",
+            nullable: true,
+            oldClrType: typeof(string),
+            oldType: "nvarchar(max)");
+
+        migrationBuilder.AlterColumn<int>(
+            name: "CreatedById",
+            table: "Product",
+            type: "int",
+            nullable: true,
+            oldClrType: typeof(int),
+            oldType: "int");
+
+        migrationBuilder.AlterColumn<int>(
+            name: "BrandId",
+            table: "Product",
+            type: "int",
+            nullable: true,
+            oldClrType: typeof(int),
+            oldType: "int");
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AlterColumn<string>(
+            name: "Variants",
+            table: "Product",
+            type: "nvarchar(max)",
+            nullable: false,
+            defaultValue: "",
+            oldClrType: typeof(string),
+            oldType: "nvarchar(max)",
+            oldNullable: true);
+
+        migrationBuilder.AlterColumn<int>(
+            name: "CreatedById",
+            table: "Product",
+            type: "int",
+            nullable: false,
+            defaultValue: 0,
+            oldClrType: typeof(int),
+            oldType: "int",
+            oldNullable: true);
+
+        migrationBuilder.AlterColumn<int>(
+            name: "BrandId",
+            table: "Product",
+            type: "int",
+            nullable: false,
+            defaultValue: 0,
+            oldClrType: typeof(int),
+            oldType: "int",
+            oldNullable: true);
+    }
+}
