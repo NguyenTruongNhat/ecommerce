e374a77410684268227d75b87792c8127010ca47
NguyenTruongNhat <nguyentruongnhat9599@gmail.com>
2025-12-03 22:06:25 +0700

    Add Migration

---
+++ Ecommerce/src/Ecommerce.Persistence/Migrations/20251203150518_UpdateRefreshTokenEntity.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ecommerce.Persistence.Migrations;
+
+/// <inheritdoc />
+public partial class UpdateRefreshTokenEntity : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AddColumn<int>(
+            name: "Id",
+            table: "RefreshToken",
+            type: "int",
+            nullable: false,
+            defaultValue: 0);
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "Id",
+            table: "RefreshToken");
+    }
+}
